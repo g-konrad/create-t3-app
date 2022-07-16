aeb3aca1fa1b993b3fa6fad76f575aa4ac882e8e
Gustavo Konrad <g.konrad@outlook.com>
2022-07-16 12:58:19 -0300

    refactor: changing welcome page to something similar to vanilla t3

---
+++ template/studs/tech/with-tw.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  export let name: string
+  export let description: string
+  export let documentation: string
+</script>
+
+<div class="hover:scale-105 cursor-pointer duration-500 flex flex-col justify-center items-center text-center rounded shadow-xl border-2 border-gray-500 h-full w-full p-6">
+  <h2 class="text-lg text-gray-700">{name}</h2>
+  <p class="text-sm text-gray-600">{description}</p>
+  <a
+    class="text-sm text-violet-500 underline decoration-dotted underline-offset-2 cursor-pointer mt-3"
+    href={documentation}
+    target="_blank"
+    rel="noreferrer"
+  >
+    Documentation
+  </a>
+</div>
